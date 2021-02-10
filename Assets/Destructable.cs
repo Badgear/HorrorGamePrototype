876f8f98b3f8003c1ef44592a5f1d281421e91a3
Dima <dmitrypopov31@gmail.com>
2021-02-10 20:20:46 +0300

    initial commit

---
+++ Assets/Destructable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+   
+   void OnMouseDpwn ()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
